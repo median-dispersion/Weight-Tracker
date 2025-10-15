d4b05298196c05125df44b7e6bcc4339cea8f86a
median-dispersion <mediandispersion@gmail.com>
2025-10-15 12:23:07 +0200

    Added header element and style

--- Client/Elements/elements.js
+++ Client/Elements/elements.js
@@ -1,5 +1,7 @@
 // Import element definitions
 import IconElement from "./IconElement.js";
+import HeaderElement from "./HeaderElement.js";
 
 // Register custom elements
 customElements.define("icon-element", IconElement);
+customElements.define("header-element", HeaderElement);
